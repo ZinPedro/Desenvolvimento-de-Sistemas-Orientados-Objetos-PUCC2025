d6a4aae5b08653d1cc36ff79ce854994627dbbde
Pedro Henrique Coan Zin <115580352+ZinPedro@users.noreply.github.com>
2025-09-03 14:46:19 -0300

    lab 04 (continuação 03)

---
+++ Lab04/Account.java
@@ -0,0 +1,88 @@
+//Fazer Saque, Trocar Senha (conheça a senha antiga) ...
+
+public class Account {
+    private String name,senha; //variavel de instancia
+    private int numConta; //variavel de instancia
+    private double balance, limite;  //variavel de instancia
+    
+    //contrutor de account que recebe dois parametros
+    public Account(String name,int numConta, double limite){
+
+        this.name = name; //atribui name variavel de intancia name
+        this.numConta = numConta;
+        this.senha = "0000";
+        
+
+        //valida que o balance eh maior que 0.0; se nao for,
+        // a variavel de instancia balance mantem seu valor inicial padrão de 0.0
+        if (balance > 0.0){ //se o saldo for valido
+            this.balance = 0; //o atribui à variavel de intancia balance 
+        }
+        if (limite > 0.0){ //se o saldo for valido
+            this.limite = limite; //o atribui à variavel de intancia balance 
+        }
+    }
+
+    public Account(){
+        this.name = ""; //atribui name variavel de intancia name
+        this.numConta = 0;
+        this.senha = "";
+        
+
+        //valida que o balance eh maior que 0.0; se nao for,
+        // a variavel de instancia balance mantem seu valor inicial padrão de 0.0
+        if (balance > 0.0){ //se o saldo for valido
+            this.balance = 0; //o atribui à variavel de intancia balance 
+        }
+        if (limite > 0.0){ //se o saldo for valido
+            this.limite = 0; //o atribui à variavel de intancia balance 
+        }
+    }
+
+    //método que deposita (adciona) apenas uma quantia válida no saldo
+    public void deposit (double depositAmount){
+
+        if (depositAmount > 0.0) //se depositAmount for valido
+            this.balance = this.balance + depositAmount; //o adiciona saldo
+            
+    }
+
+    public void Imprime(){
+        System.out.println("\nDados:\n Nome: " + this.getName() + "\nNumero da conta:" + this.getNumConta() + "\nSaldo: " + this.getBalance() + "\nLimite: " + this.getLimite() + "\n");
+    }
+
+    //método retorna o saldo da conta
+    public double getBalance(){
+        return balance;
+    }
+
+    //método que define o nome
+    public void setName(String name){
+        this.name = name;
+    }
+
+    //método que retorna o nome
+    public String getName(){
+        return name; //retorna o valor de name ao chamador
+    }//fim do método getName
+
+    public void setNumConta(int numConta){
+        this.numConta = numConta;
+    }
+
+    public int getNumConta(){
+        return numConta;
+    }
+
+    public void setLimite(double limite){
+        this.limite = limite;
+    }
+
+    public double getLimite(){
+        return limite;
+    }
+
+
+
+
+} //Fim da classe Account
---
+++ Lab04/AccountTest.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class AccountTest{
+    public static void main (String[] args){
+        Account account1 = new Account("Pedro",1,100.00);
+        Account account2 = new Account("Thiago",2,100000.00);
+        Account account3 = new Account();
+        account1.Imprime();
+        account2.Imprime();
+        account3.Imprime();
+        account1.deposit(50.0);
+        account3.deposit(50.0);
+        account1.Imprime();
+        account2.Imprime();
+        account3.Imprime();
+        account3.setName("Sabino");
+        account3.Imprime();
+        /* 
+
+        //exibe saldo inicial de cada objeto
+        System.out.printf("%s balance: $%.2f %n",
+            account1.getName(),account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n",
+            account2.getName(),account2.getBalance());
+
+        //cria um Scanner para obter entrada a partir da janela de comando
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Enter deposit amount for account1: "); //prompt
+        double depositAmount = input.nextDouble(); //obtém a entrada do usuario
+        System.out.printf("%nadding %.2f to account balance %n%n", depositAmount);
+        account1.deposit(depositAmount); //adiciona o saldo de account1
+        //exibe os saldos
+        System.out.printf("%s balance: $%.2f %n", account1.getName(),account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n", account2.getName(),account2.getBalance());
+        
+        System.out.printf("Enter deposit amount for account2: "); //prompt
+        depositAmount = input.nextDouble(); //obtém a entrada do usuário
+        System.out.printf("%nadding %.2f to account2 balance %n%n",depositAmount);
+        account2.deposit(depositAmount); //Adciona ao saldo de account 2
+
+        //exibe os saldos
+        System.out.printf("%s balance: $%.2 %n",account1.getName(),account1.getBalance());
+        System.out.printf("%s balance: $%.2f %n%n",account2.getName(),account2.getBalance());
+        */
+
+    }//fim da main
+}  //fim da classe AccountTest
